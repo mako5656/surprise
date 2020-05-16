cbff540e7d6260d84ab7464f4a118391285b77d5
mako5656 <makoto09142425@gmail.com>
2020-05-16 16:24:41 +0900

    comment out

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,11 @@ from .views import ItemFilterView, ItemDetailView, ItemCreateView, ItemUpdateVie
 from . import cover
 
 urlpatterns = [
-    path('surprise/',  ItemFilterView.as_view(), name='index'),
-    path('create/', ItemCreateView.as_view(), name='create'),
-    path('detail/<int:pk>/', ItemDetailView.as_view(), name='detail'),
-    path('update/<int:pk>/', ItemUpdateView.as_view(), name='update'),
-    path('delete/<int:pk>/', ItemDeleteView.as_view(), name='delete'),
-
     path('', cover.post_list, name='post_list'),
     path('cover2/', cover.post_list2, name='post_list2'),
+    path('surprise/',  ItemFilterView.as_view(), name='index'),
+    # path('create/', ItemCreateView.as_view(), name='create'),
+    # path('detail/<int:pk>/', ItemDetailView.as_view(), name='detail'),
+    # path('update/<int:pk>/', ItemUpdateView.as_view(), name='update'),
+    # path('delete/<int:pk>/', ItemDeleteView.as_view(), name='delete'),
 ]
